91065c55e2967f66abf95bca69256809983eec80
Bridger Middleton <bmiddy3@gmail.com>
2019-11-05 16:55:14 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotingDojo.Models;
+
+namespace QuotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult AddQuotePage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddQuote(Quote yourQuote)
+        {
+            string query = $"INSERT INTO Quote (Name, TheQuote, created_at, updated_at) VALUES ('{yourQuote.Name}','{yourQuote.TheQuote}',NOW(),NOW());";
+            DbConnector.Execute(query);
+            return RedirectToAction("AllTheQuotesPage");
+        }
+        [HttpGet("quotes")]
+        public IActionResult AllTheQuotesPage()
+        {
+            List<Dictionary<string,object>> AllQuotes = DbConnector.Query("SELECT * FROM Quote");
+            ViewBag.ListofQuotes = AllQuotes;
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Quote.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+namespace QuotingDojo.Models
+{
+    public class Quote
+    {
+        public string Name{get;set;}
+        public string TheQuote{get;set;}
+
+    }
+}
